c354b5048e91ed63f6691e9c4581949400410395
Camila Villamayor <camilalvillamayor@gmail.com>
2024-10-28 13:00:16 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { Footer } from "./components/footer/Footer";
+import { Main } from "./components/main/Main";
+import { Navbar } from "./components/navbar/Navbar";
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <Main />
+      <Footer />
+
+      <h1>Main</h1>
+    </div>
+  );
+}
+
+export default App;
